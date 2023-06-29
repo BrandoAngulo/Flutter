8d13bc548278b5fdc35dd1b9854450d2b050c22f
BrandoAngulo <steven.sm.1992@gmail.com>
2023-06-29 13:03:15 -0500

    añadido boton retroceder de la pagina de alerts con el widget navigator.pop y tambien se añadio un elevatedButton para agregarle alertas

--- project_app_1/lib/screens/alert_screen.dart
+++ project_app_1/lib/screens/alert_screen.dart
@@ -6,15 +6,18 @@ class AlertScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('AlertScreen'),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('ElevatedButton'),
+          //Realizando alertas en este boton de presionar
+          onPressed: () {},
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.close),
-        onPressed: (){},
-        backgroundColor: Colors.deepPurpleAccent,
-        ),
-        
+          child: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
     );
   }
 }
--- project_app_1/lib/theme/app_theme.dart
+++ project_app_1/lib/theme/app_theme.dart
@@ -12,19 +12,31 @@ class AppTheme {
 
       //Style para todos los botones de la app
       textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(foregroundColor: primary)));
+          style: TextButton.styleFrom(foregroundColor: primary)),
 
+      //floattingActionBotton
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: primary, elevation: 10, splashColor: Colors.black12),
+
+      //elevatedButton
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+        backgroundColor: primary,
+        shape: const StadiumBorder(),
+      )));
+
+//Dark Theme
   static final darkTheme = ThemeData.dark().copyWith(
     //color primario
     primaryColor: Colors.deepOrange,
     //tema del appBar
     appBarTheme: const AppBarTheme(color: primary, elevation: 0),
     //background del scaffold
-    scaffoldBackgroundColor: Colors.black,
-  );
-
-  //floattingActionBotton
-  static const botontheme = FloatingActionButtonThemeData(
-backgroundColor: primary
+      scaffoldBackgroundColor: Colors.black,
+      //floattingActionBotton
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.amber,
+          elevation: 10,
+          splashColor: Colors.black12)
   );  
 }
