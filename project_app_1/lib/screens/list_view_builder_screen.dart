80ff465021cf2237470bdb84ea29aedb973d4ac7
BrandoAngulo <steven.sm.1992@gmail.com>
2023-07-07 09:08:45 -0500

    se crea pulltorefresh o un refresh para cargar nuevas imagenes en el lisView

--- project_app_1/lib/screens/list_view_builder_screen.dart
+++ project_app_1/lib/screens/list_view_builder_screen.dart
@@ -45,6 +45,18 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     setState(() {});
   }
 
+   Future<void> onRefresh () async{
+    await Future.delayed(const Duration(microseconds: 3)); //funcion para esperar la carga de imagenes de 3 seg
+    //creamos variable para tomar el ultimo listado
+    final lastId = idImages.last;
+    //borramos el estado o las imagenes actuales del listview
+    idImages.clear();
+    //agregamos el ultimo id de la imagen que teniamos para crear 5 registros nuevos
+    idImages.add(lastId + 1);
+    //y llamamos la funcion que agrega 5 imagenes
+    addId5(); //esta funcion es como si fuera un return null
+  }
+
   @override
   Widget build(BuildContext context) {
     //con esta funcion nos da las propiedades para saber el ancho y el alto de la pantalla
@@ -58,23 +70,27 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
         removeTop: true,
         child: Stack(
           children: [
-            ListView.builder(
-              controller: scrollController,
-              //muestra el scroll cuando llega alo ultimo o al inicio que puede extenderlo o como si halara la pantalla
-              physics: const BouncingScrollPhysics(),
-              //es la cantidad de items que va a contener
-              itemCount: idImages.length,
-              itemBuilder: (BuildContext context, int index) {
-                return FadeInImage(
-                    //Toma todo el ancho disponible
-                    width: double.infinity,
-                    height: 300,
-                    //toma todo el espacio disponible
-                    fit: BoxFit.cover,
-                    placeholder: const AssetImage('assets/images/giphy.gif'),
-                    image: NetworkImage(
-                        'https://picsum.photos/500/300?image=${idImages[index]}'));
-              },
+            RefreshIndicator(
+              color: AppTheme.primary,
+              onRefresh: onRefresh,
+              child: ListView.builder(
+                controller: scrollController,
+                //muestra el scroll cuando llega alo ultimo o al inicio que puede extenderlo o como si halara la pantalla
+                physics: const BouncingScrollPhysics(),
+                //es la cantidad de items que va a contener
+                itemCount: idImages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeInImage(
+                      //Toma todo el ancho disponible
+                      width: double.infinity,
+                      height: 300,
+                      //toma todo el espacio disponible
+                      fit: BoxFit.cover,
+                      placeholder: const AssetImage('assets/images/giphy.gif'),
+                      image: NetworkImage(
+                          'https://picsum.photos/500/300?image=${idImages[index]}'));
+                },
+              ),
             ),
             if (isLoading)
               Positioned(
